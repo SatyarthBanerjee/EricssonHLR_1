6f1e2891d9d472f19600e791b4c4e8456e996321
Ahana <ahanade100@gmail.com>
2023-07-31 16:04:29 +0530

    env file added

--- src/Components/PostData.jsx
+++ src/Components/PostData.jsx
@@ -1,10 +1,11 @@
 import axios from "axios";
 
-const url = "http://localhost:5000";
+// const url = "http://localhost:5000";
+const url2 = `http://${process.env.ENDPOINT}:${process.env.PORT}`;
 
 export const postData = async (data) => {
   try {
-    let res = await axios.post(`${url}/new-data`, data);
+    let res = await axios.post(`${url2}/new-data`, data);
     console.log("Data saved: ", res.data);
   } catch (error) {
     console.log("Error calling postData--> ", error.message);
